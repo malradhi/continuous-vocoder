fc9fa6af0faa425284259e68a76fb7f744089324
malradhi <cemohammedsalah@gmail.com>
2020-01-29 17:10:44 +0100

    shell file

---
+++ run_continuous_vocoder.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+#################################################################################################################
+#																												#
+#       Continuous vocoder based Merlin toolkit																	#
+#       Works only with 16 kHz WAV files																		#
+#																												#
+#       Author:																									#
+#       Tamas Gabor CSAPO, csapot@tmit.bme.hu																	#
+#       Mohammed Salah Al-Radhi, malradhi@tmit.bme.hu															#
+#																												#
+#		requirement: SPTK 3.8 or above in PATH folder															#
+#																												#
+#		Reference																								#
+#	     [1] Mohammed Salah Al-Radhi, Tamás Gábor Csapó, Géza Németh, Time-domain envelope						#
+#	         modulating the noise component of excitation in a continuous residual-based vocoder				#
+#	         for statistical parametric speech synthesis, in Proceedings of the 18th Interspeech conference,	#
+#	         pp. 434-438, Stockholm, Sweeden, 2017.																#
+#																												#
+#################################################################################################################
+
+
+# add permission for executing below files
+chmod u+x *.sh
+
+
+# step 1: extract continuous features
+./01_analysis.sh
+
+
+# step 2: synthesize speech
+./02_synthesis.sh
+
+
+echo "Done!"
+echo ""
